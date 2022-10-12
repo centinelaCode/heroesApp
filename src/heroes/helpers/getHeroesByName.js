4b63154e7c8eda602ba296c9614c85656d1b7675
centinela1024tb <centinela1024tb@gmail.com>
2022-10-12 15:20:59 -0500

    [ADD] Se implementa un helper para getHeroesByName, se le pasa como parametro 'name'
    => name es loq ue sde escribio en el input de search

---
+++ src/heroes/helpers/getHeroesByName.js
@@ -0,0 +1,12 @@
+import { heroes } from "../data/heroes";
+
+export const getHeroesByName = ( name = '' ) => {
+  // convertimos e minusculas y quitamos espacios al inicio y al final
+  name = name.toLocaleLowerCase().trim();
+
+  //no existe termino de busqueda
+  if( name.length === 0) return [];
+
+  // y filtramos lo que hacen match con la busqueda
+  return heroes.filter( hero => hero.superhero.toLocaleLowerCase() .includes(name) );
+}
