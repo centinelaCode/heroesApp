cb980908700e4e10b29a9438be5a2422eac57c64
centinela1024tb <centinela1024tb@gmail.com>
2022-10-12 12:55:19 -0500

    [ADD] Se empieza a trabajar en el componente HeroPage que mostrará la info del hero
    => Se obtiene el hero por el param en la url
    => Se obtiene con el hook useParamas de recat-router-dom

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,20 @@
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById} from '../helpers'
 
 export const HeroPage = () => {
+
+  // obtenemos el id medinate los params que viene en la url
+  const { id } = useParams();
+  
+  // buscamos el hero en base al id
+  const hero = getHeroById(id);
+
+  // si no existe el hero nos saca de esa route (en caso de que alteren manuelmente la url)
+  if(!hero) {
+    return <Navigate to="/marvel" />
+  }
+  
+
   return (
     <>
       <h1>Hero by ID</h1>
